7821915d957314172e34969412aa5146c63f4366
Olen Latham <olen@latham.cloud>
2023-06-15 00:06:09 -0400

    Update db.js
    Corrected sqlite error and added logging so if there isn't a db it will create one

--- modules/db.js
+++ modules/db.js
@@ -4,14 +4,14 @@ const { countTokens } = require('./tokenHelper');
 
 const { getCodeBaseAutopilotDirectory } = require('./autopilotConfig');
 
-DB_FILE_NAME = 'autopilot.db'
+const DB_FILE_NAME = 'autopilot.db';
 
 /**
- * @description Creates the files table
- * @param {sqlite3.Database} db - The database to create the table in
+ * @description Creates the "files" table in the SQLite database if it doesn't exist
+ * @param {sqlite3.Database} db - The database connection
  */
-function createFilesTable(db){
-    const sql =`
+function createFilesTable(db) {
+  const sql = `
 CREATE TABLE IF NOT EXISTS files (
     path TEXT PRIMARY KEY,
     tokensCount INTEGER,
@@ -21,34 +21,49 @@ CREATE TABLE IF NOT EXISTS files (
     timestamp INTEGER,
     dependenciesLibs TEXT
 );
-`
-    db.run(sql);
+`;
+  db.run(sql, [], function (err) {
+    if (err) {
+      console.error('Error creating "files" table:', err);
+    } else {
+      console.log('The "files" table has been created successfully.');
+    }
+  });
 }
 
 /**
- * @description Creates the files table
- * @param {string} codeBaseAutopilotDirectory - The path to the .autopilot directory of the codebase
-*/
-function createDB(codeBaseDirectory){
-    const db = getDB(codeBaseDirectory)
-    createFilesTable(db);
-}
-
-function getDBFilePath(codeBaseDirectory){
-    codeBaseAutopilotDirectory = getCodeBaseAutopilotDirectory(codeBaseDirectory);
-    dbFilePath = path.posix.join(codeBaseAutopilotDirectory, DB_FILE_NAME);
-    return dbFilePath;
-}
-
-/**
- * @description Creates the files table
- * @param {string} codeBaseAutopilotDirectory
- * @returns {sqlite3.Database} db
+ * @description Creates the SQLite database and the "files" table if they don't exist
+ * @param {string} codeBaseDirectory - The path to the .autopilot directory of the codebase
  */
-function getDB(codeBaseDirectory){
-    dbFilePath = getDBFilePath(codeBaseDirectory);
-    const db = new sqlite3.Database(dbFilePath);
-    return db;
+function createDB(codeBaseDirectory) {
+  const db = getDB(codeBaseDirectory);
+  createFilesTable(db);
+}
+
+/**
+ * @description Gets the file path of the SQLite database
+ * @param {string} codeBaseDirectory
+ * @returns {string} - The file path of the SQLite database
+ */
+function getDBFilePath(codeBaseDirectory) {
+  const codeBaseAutopilotDirectory = getCodeBaseAutopilotDirectory(codeBaseDirectory);
+  const dbFilePath = path.join(codeBaseAutopilotDirectory, DB_FILE_NAME);
+  return dbFilePath;
+}
+
+/**
+ * @description Creates the SQLite database and returns the database connection
+ * @param {string} codeBaseDirectory - The path to the .autopilot directory of the codebase
+ * @returns {sqlite3.Database} - The database connection
+ */
+function getDB(codeBaseDirectory) {
+  const dbFilePath = getDBFilePath(codeBaseDirectory);
+  const db = new sqlite3.Database(dbFilePath);
+  db.serialize(() => {
+    createFilesTable(db);
+  });
+  console.log(`Connected to SQLite database: ${dbFilePath}`);
+  return db;
 }
 
 /**
@@ -57,18 +72,21 @@ function getDB(codeBaseDirectory){
  * @param {string} codeBaseDirectory - The absolute path to the code base directory containing the SQLite database.
  * @param {string} filePath - The absolute path to the file to be deleted.
  */
-function deleteFile(codeBaseDirectory, filePath){
-    db = getDB(codeBaseDirectory);
-    const sql = `
-delete from files 
-where path = ?`
-    db.run(sql, [filePath]);
+function deleteFile(codeBaseDirectory, filePath) {
+  const db = getDB(codeBaseDirectory);
+  const sql = `DELETE FROM files WHERE path = ?`;
+  db.run(sql, [filePath], function (err) {
+    if (err) {
+      console.error('Error deleting file:', err);
+    } else {
+      console.log(`File ${filePath} deleted successfully from the "files" table.`);
+    }
+  });
 }
 
-
 /**
- * @description Inserts or updates a file in the files table
- * @param {sqlite3.Database} db - The database to insert the file into
+ * @description Inserts or updates a file in the "files" table
+ * @param {string} codeBaseDirectory - The path to the .autopilot directory of the codebase
  * @param {object} file - The file to insert or update
  * @param {string} file.filePath - The relative path of the file
  * @param {string} file.fileContent - The content of the file
@@ -78,10 +96,10 @@ where path = ?`
  * @param {string} summary - The summary of the file
  * @param {string} dependenciesLibs - The dependencies of the file
  */
-function insertOrUpdateFile(codeBaseDirectory, file, summary, dependenciesLibs){
-    db = getDB(codeBaseDirectory);
-    const summaryTokensCount = countTokens(summary);
-    const sql = `
+function insertOrUpdateFile(codeBaseDirectory, file, summary, dependenciesLibs) {
+  const db = getDB(codeBaseDirectory);
+  const summaryTokensCount = countTokens(summary);
+  const sql = `
 INSERT OR REPLACE INTO files (
     path, 
     tokensCount,
@@ -91,46 +109,56 @@ INSERT OR REPLACE INTO files (
     timestamp,
     dependenciesLibs)
 VALUES (?, ?, ?, ?, ?, ?, ?)
-`
-    db.run(sql, [
-        file.filePath, 
-        file.fileTokensCount, 
-        summary,
-        summaryTokensCount,
-        file.fileHash,
-        file.fileTimestamp,
-        dependenciesLibs
-    ]);
+`;
+  db.run(sql, [
+    file.filePath,
+    file.fileTokensCount,
+    summary,
+    summaryTokensCount,
+    file.fileHash,
+    file.fileTimestamp,
+    dependenciesLibs
+  ], function (err) {
+    if (err) {
+      console.error('Error inserting/updating file:', err);
+    } else {
+      console.log(`File ${file.filePath} inserted/updated successfully in the "files" table.`);
+    }
+  });
 }
 
 /**
- * @description Gets all files from the files table
+ * @description Gets all files from the "files" table
  * @param {string} codeBaseDirectory - The path to the codebase
- * @returns {Array<{
-    * path: string, // The relative path of the file.
-    * hash: string, // The hash of the file content.
-    * timestamp: string // The timestamp when the file was last modified.
- * }>} - An array of objects containing file details retrieved from the directory.
+ * @returns {Promise<Array<{
+    path: string, // The relative path of the file.
+    hash: string, // The hash of the file content.
+    timestamp: string // The timestamp when the file was last modified.
+ }>>} - An array of objects containing file details retrieved from the directory.
  * @throws {Error} If an error occurs during the database query.
-*/
-async function getDBFiles(codeBaseDirectory){
-    db = getDB(codeBaseDirectory);
-    const sql = `
-SELECT path, hash, timestamp 
-FROM files
-`
-    files = await new Promise((resolve, reject) => {
-        db.all(sql, (err, rows) => {
-            if (err) {
-            reject(err);
-            } else {
-            resolve(rows);
-            }
-        });
+ */
+async function getDBFiles(codeBaseDirectory) {
+  const db = getDB(codeBaseDirectory);
+  const sql = `SELECT path, hash, timestamp FROM files`;
+  const files = await new Promise((resolve, reject) => {
+    db.all(sql, (err, rows) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(rows);
+      }
     });
-    return files;
+  });
+  console.log('Retrieved files from the "files" table:', files);
+  return files;
 }
-    
 
-
-module.exports = { createDB, createFilesTable, insertOrUpdateFile, getDB, getDBFiles, deleteFile, getDBFilePath }
+module.exports = {
+  createDB, // Creates the SQLite database and the "files" table if they don't exist
+  createFilesTable, // Creates the "files" table in the SQLite database if it doesn't exist
+  insertOrUpdateFile, // Inserts or updates a file in the "files" table
+  getDB, // Gets the SQLite database connection
+  getDBFiles, // Gets all files from the "files" table
+  deleteFile, // Deletes a file from the "files" table
+  getDBFilePath // Gets the file path of the SQLite database
+};
